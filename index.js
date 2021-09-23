ec705487349d1dde2f592663d473aeea1b181a83
Rohail Rafique <rohailrafique@gmail.com>
2021-09-22 22:57:35 -0400

    commit tree 321c12017deca3e77b554dd815827b71058780b2

---
+++ index.js
@@ -0,0 +1,18 @@
+// describe("index.js", () => {
+//     it("has a function expression called divide", () => {
+//       expect(divide).to.exist
+//     })
+
+const divide = () => 2000/100;
+
+// it("has an arrow function called square", () => {
+//     expect(square).to.exist
+//   })
+
+const square = (x) => x ** 2 ;
+
+// it("has an arrow function called add", () => {
+//     expect(add).to.exist
+//   })
+
+const add = (a, b) => a + b; 
